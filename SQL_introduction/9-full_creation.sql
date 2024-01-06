7598d65d4381f57e35fddebf78a927d9a27d1208
fageer <fageern@gmail.com>
2024-01-06 21:31:24 +0300

    9. Full creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS second_table
 
 -- insert into second_table some rows
 INSERT INTO second_table
-VALUES (id = 1, name = "John", score = 10),
-       (id = 2, name = "Alex", score = 3),
-       (id = 3, name = "Bob", score = 14),
-       (id = 4, name = "George", score = 8);
+VALUES (1, "John", 10),
+       (2, "Alex", 3),
+       (3, "Bob", 14),
+       (4, "George", 8);
