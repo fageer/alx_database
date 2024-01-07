3aef579bbda4aea14131c48d8e6b5c9dce0163bf
fageer <fageern@gmail.com>
2024-01-07 22:17:14 +0300

    7. Cities table

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- create the database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- create the table cities
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
+(
+    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    state_id INT NOT NULL FOREIGN KEY REFERENCES states.id,
+    name VARCHAR(256) NOT NULL
+);
