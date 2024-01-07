09fed8bafd8af98a7c063f18d29f2944a14f9b28
fageer <fageern@gmail.com>
2024-01-07 22:29:59 +0300

    7. Cities table

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,13 +1,14 @@
 -- create the database hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
--- Switch to the created database
+-- Switch to the hbtn_0d_usa database
 USE hbtn_0d_usa;
 
 -- create the table cities
 CREATE TABLE IF NOT EXISTS cities
 (
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    state_id INT NOT NULL FOREIGN KEY REFERENCES states(id),
-    name VARCHAR(256) NOT NULL
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY(state_id) REFERENCES states(id)
 );
