df93f2b7f761f36fa5023223f434c7a824f79522
fageer <fageern@gmail.com>
2024-01-07 22:07:14 +0300

    6. States table

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 -- create the table states
-CREATE TABLE IF NOT EXISTS states
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
 (
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name VARCHAR(256) NOT NULL
